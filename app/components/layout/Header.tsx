cfc6d25763bb8503e00ca5ad57d3d4665d99c7aa
Talha Muhammad <121675123+talha1230@users.noreply.github.com>
2024-09-29 02:19:05 +0800

    Refactor .gitignore and update Header component

--- app/components/layout/Header.tsx
+++ app/components/layout/Header.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Header: React.FC = () => {
   return (
 	<header className="bg-gray-800 text-white p-4">
-	  <h1 className="text-2xl">My WebApp</h1>
+	  <h1 className="text-2xl">Supposed to be Navbar</h1>
 	</header>
   );
 };
